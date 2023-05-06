6ab9423aa866769734518d0f6fc5274faf49195c
Emilian Bochenek <bchn77@icloud.com>
2023-05-06 11:39:25 +0200

    dodano tlumaczenie

---
+++ Zadanie 2/bestpriceunder/translations/pl.php
@@ -0,0 +1,7 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{bestpriceunder}prestashop>bestpriceunder_788c14326db8a287639459c6de0b30cb'] = 'Najlepsza cena poniżej 100zł';
+$_MODULE['<{bestpriceunder}prestashop>bestpriceunder_36dde681a11dbd7303e303382e0f15c9'] = 'Dodaje flage \"Najlepsza cena w sieci\"';
+$_MODULE['<{bestpriceunder}prestashop>bestpriceunder_687ce84141fccdbab1381c4474210064'] = 'Najlepsza cena w sieci';
